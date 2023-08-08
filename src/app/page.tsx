cc571af5428fd65e1232c4fbcd947e5fa005636d
sc30gsw <haidoushi875@gmail.com>
2023-08-09 08:44:52 +0900

    レイアウトの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { getAllTodos } from 'src/api'
 
 import AddTask from './components/AddTask'
@@ -7,12 +6,10 @@ import TodoList from './components/TodoList'
 const Home = async () => {
   const todos = await getAllTodos()
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
-      <h1 className="text-4xl font-bold text-gray-700 -mt-32">
-        Next 13 Todo App
-      </h1>
+    <main className="h-screen flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700">Next 13 Todo App</h1>
       <div className="w-full max-w-xl mt-5">
-        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg overflow-y-auto max-h-[calc(100vh-10rem)]">
           <AddTask />
           <TodoList todos={todos} />
         </div>
